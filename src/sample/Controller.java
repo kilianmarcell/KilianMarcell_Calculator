8518358a134a4eb000d0b3b72185b8f56d0c8e81
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-02 11:55:00 +0100

    Összeadás kész

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    public Label vegeredmeny;
+    @FXML
+    public TextField elsoBeviteliMezo;
+    @FXML
+    public TextField masodikBeviteliMezo;
+
+    @FXML
+    public void osszeadas(ActionEvent actionEvent) {
+        int elsoSzam = Integer.parseInt(elsoBeviteliMezo.getText());
+        int masodikSzam = Integer.parseInt(masodikBeviteliMezo.getText());
+        vegeredmeny.setText(elsoSzam + masodikSzam + "");
+    }
+
+    @FXML
+    public void kivonas(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void szorzas(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void osztas(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void maradekosOsztas(ActionEvent actionEvent) {
+    }
 }
