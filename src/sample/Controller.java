70ba20bc332fbe4db1615e637c937ddd46554d57
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-03 13:08:04 +0100

    TODO: Bemeneti karakter csak szám lehessen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class Controller {
     @FXML
